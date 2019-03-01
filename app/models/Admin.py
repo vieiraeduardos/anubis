20d413ca854aa75b2659622babccc4ab8b9ce4a0
Eduardo <edusvieirap@gmail.com>
2019-03-01 11:49:25 -0300

    Implementing create method at Admin.py

--- app/models/Admin.py
+++ app/models/Admin.py
@@ -1,8 +1,29 @@
 from app.models.DatabaseFactory import DatabaseFactory
 
 class Admin():
-    def __init__(self):
+    def __init__(self, cpf="", name="", email="", password="", createdAt="", modifiedAt=""):
+        self.cpf = cpf
+        self.name = name
+        self.email = email
+        self.password = password
+        self.createdAt = createdAt
+        self.modifiedAt = modifiedAt
         self.connection = DatabaseFactory().getConnection()
+
+    def create(self):
+        try:
+          with self.connection.cursor() as cursor:
+            sql = "INSERT INTO admins(cpf, name, email, password, createdAt, modifiedAt) VALUES (%s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql, (self.cpf, self.name, self.email, self.password, self.createdAt, self.modifiedAt))
+            cursor.commit()
+
+            return True
+        except Exception as e:
+            print(e)
+            return False
+        finally:
+          self.connection.close()
+
 
     def getAdminByEmail(self, email):
         try:
