5818d54ea35168b551a371685db322dcb2424fb5
Eduardo <edusvieirap@gmail.com>
2019-03-01 11:29:51 -0300

    adding Database.py

---
+++ app/models/DatabaseFactory.py
@@ -0,0 +1,12 @@
+import pymysql.cursors
+
+class DatabaseFactory():
+  def __init__(self):
+    self.connection = pymysql.connect(host='localhost',
+                                 user='root',
+                                 password='mysql',
+                                 db='anubisdb',
+                                 charset='utf8mb4',     cursorclass=pymysql.cursors.DictCursor)
+
+  def getConnection(self):
+    return self.connection
