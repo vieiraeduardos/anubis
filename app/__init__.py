763e99bbbd69740a1973d283a43641bf4cc7577c
Eduardo <edusvieirap@gmail.com>
2019-03-01 11:27:46 -0300

    adding route ton register-page.html

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+from app.controllers import AdminController
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -9,3 +11,7 @@ def index():
 @app.route("/login/", methods=["GET"])
 def redirect_login():
     return render_template("pages/login-page.html")
+
+@app.route("/signup/", methods=["GET"])
+def redirect_login():
+    return render_template("pages/register-page.html")
