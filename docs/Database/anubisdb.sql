8e1bd4deafc442b16d99aba0da803c7c63b81924
Eduardo <edusvieirap@gmail.com>
2019-03-01 11:21:38 -0300

    Updating database file

--- docs/Database/anubisdb.sql
+++ docs/Database/anubisdb.sql
@@ -1,5 +1,7 @@
 CREATE DATABASE anubisdb;
 
+USE anubisdb;
+
 CREATE TABLE judges(
 	cpf VARCHAR(13) NOT NULL PRIMARY KEY UNIQUE,
 	name VARCHAR(255) NOT NULL,
@@ -7,8 +9,18 @@ CREATE TABLE judges(
 	password VARCHAR(255) NOT NULL,
 	createdAt DATE NOT NULL,
 	modifiedAt DATE NOT NULL
-	
-); 
+
+);
+
+CREATE TABLE admins (
+	cpf VARCHAR(13) NOT NULL PRIMARY KEY UNIQUE,
+	name VARCHAR(255) NOT NULL,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	password VARCHAR(255) NOT NULL,
+	createdAt DATE NOT NULL,
+	modifiedAt DATE NOT NULL
+
+);
 
 CREATE TABLE authors(
 	cpf VARCHAR(13) NOT NULL PRIMARY KEY UNIQUE,
@@ -25,15 +37,17 @@ CREATE TABLE papers (
 	title VARCHAR(255) NOT NULL,
 	author INTEGER NOT NULL,
 	category INTEGER NOT NULL,
-	subcategory INTEGER NOT NULL, 
+	subcategory INTEGER NOT NULL,
 	isExposed BOOLEAN NOT NULL,
 	isPresented BOOLEAN NOT NULL,
+	judge INTEGER NOT NULL,
 	createdAt DATE NOT NULL,
 	modifiedAt DATE NOT NULL,
 
 	FOREIGN KEY(author) REFERENCES authors(cpf),
 	FOREIGN KEY(category) REFERENCES categories(code),
-	FOREIGN KEY(subcategory) REFERENCES subcategories(code)
+	FOREIGN KEY(subcategory) REFERENCES subcategories(code),
+	FOREIGN KEY(judge) REFERENCES judges(cpf)
 
 );
 
@@ -68,5 +82,3 @@ INSERT INTO subcategories(name, category) VALUES ("Tecnologias disruptivas em Sa
 INSERT INTO subcategories(name, category) VALUES ("Teleassistência / Telecuidado", 2);
 INSERT INTO subcategories(name, category) VALUES ("Telemedicina e Telessaúde aplicadas à Gestão na Saúde", 2);
 INSERT INTO subcategories(name, category) VALUES ("Legislação e Normas em Telemedicina, Telessaúde e Saúde Digital", 1);
-
-
