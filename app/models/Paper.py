6c4920e34c308d0790138d5d4444d8e72f0dcff2
Eduardo <edusvieirap@gmail.com>
2019-03-03 22:06:01 -0300

    Implementing method create paper at Paper.py

---
+++ app/models/Paper.py
@@ -0,0 +1,28 @@
+from app.models.DatabaseFactory import DatabaseFactory
+
+class Paper():
+    def __init__(self, title="", abstract="", author="", category="", subcategory="", isExposed="", isPresented="", createdAt="", modifiedAt=""):
+        self.title = title
+        self.abstract = abstract
+        self.author = author
+        self.category = category
+        self.subcategory = subcategory
+        self.isExposed = isExposed
+        self.isPresented = isPresented
+        self.createdAt = createdAt
+        self.modifiedAt = modifiedAt
+        self.connection = DatabaseFactory().getConnection()
+
+    def create(self):
+        try:
+          with self.connection.cursor() as cursor:
+            sql = "INSERT INTO papers(title, abstract, author, category, subcategory, isExposed, isPresented, createdAt, modifiedAt) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+            cursor.execute(sql, (self.title, self.abstract, self.author, self.category, self.subcategory, self.isExposed, self.isPresented, self.createdAt, self.modifiedAt))
+            self.connection.commit()
+
+            return True
+        except Exception as e:
+            print(e)
+            return False
+        finally:
+          self.connection.close()
