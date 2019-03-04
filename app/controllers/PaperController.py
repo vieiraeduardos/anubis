e701ad80aa47aa175aca0b14b807c6dcd60d225f
Eduardo <edusvieirap@gmail.com>
2019-03-03 22:00:48 -0300

    Implementing method create at PaperController.py

--- app/controllers/PaperController.py
+++ app/controllers/PaperController.py
@@ -1,7 +1,37 @@
-from flask import render_template
+from flask import render_template, request, redirect
+
+from datetime import date
 
 from app import app
 
-@app.route("/events/new/", methods=["GET"])
+from app.models.Author import Author
+from app.models.Category import Category
+from app.models.Subcategory import Subcategory
+from app.models.Paper import Paper
+
+@app.route("/papers/new/", methods=["GET"])
 def redirect_new_paper():
-    return render_template("new-paper.html")
+    authors = Author().getAllAuthors()
+    categories = Category().getAllCategories()
+    subcategories = Subcategory().getAllSubcategories()
+
+    return render_template("new-paper.html", authors=authors, categories=categories, subcategories=subcategories)
+
+
+@app.route("/papers/", methods=["POST"])
+def create_paper():
+    title = request.form.get("title")
+    abstract = request.form.get("abstract")
+    author = request.form.get("author")
+    category = request.form.get("category")
+    subcategory = request.form.get("subcategory")
+    isPresented = 1 if request.form.get("isPresented") == "1" else 0
+    isExposed = 1 if request.form.get("isExposed") == "1" else 0
+    createdAt = str(date.today())
+    modifiedAt = str(date.today())
+
+    paper = Paper(title=title, abstract=abstract, author=author, category=category, subcategory=subcategory, isExposed=isExposed, isPresented=isPresented, createdAt=createdAt, modifiedAt=modifiedAt)
+
+    paper.create()
+
+    return redirect("/")
